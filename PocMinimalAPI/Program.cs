51d645a5534a61fa53ca7e0f8db243030891a8b8
Erick Sales <ericksalec@gmail.com>
2022-06-10 00:37:38 -0300

    feat: implmentando get e post com mini validator

--- PocMinimalAPI/Program.cs
+++ PocMinimalAPI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MiniValidation;
+using PocMinimalAPI.Data;
+using PocMinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -12,6 +20,46 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/fornecedor", async (MinimalContextDb context) =>
+        await context.Fornecedores.ToListAsync())
+    .WithName("GetFornecedor")
+    .WithTags("Fornecedor");
+
+
+app.MapGet("/fornecedor/{id}", async (
+            Guid id,
+            MinimalContextDb context) =>
+
+        await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+            ? Results.Ok(fornecedor)
+            : Results.NotFound())
+    .Produces<Fornecedor>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetFornecedorPorId")
+    .WithTags("Fornecedor");
+
+
+app.MapPost("/fornecedor", async (
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+{
+    if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+
+    context.Fornecedores.Add(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id }, fornecedor)
+        : Results.BadRequest("Houve um problema ao salvar registro");
+
+}).ProducesValidationProblem()
+    .Produces<Fornecedor>(StatusCodes.Status201Created)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("PostFornecedor")
+    .WithTags("Fornecedor");
 
 app.Run();
 
